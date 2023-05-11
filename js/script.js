cdec93106d28410c58ea013b1ace27e7955e97af
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-05-11 17:26:15 +0000

    template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jakub Malhotra
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  let text = ""
+  let counter = 0
+  const multiplier = parseInt(document.getElementById("multiplier").value)
+  const multiplicand = parseInt(document.getElementById("multiplicand").value)
+
+
+  while (counter <= multiplier) {
+    text = text + multiplicand + " x " + counter + " = " + counter * multiplicand + "<br>"
+    counter = counter + 1
+  }
+  document.getElementById("answer").innerHTML = text
+}
